8bd066c59af96e1ad6872ffeb32c4b3cea3bf818
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-02-01 23:19:07 +0000

    chopin: Dump prebuilt bootctrl from MIUI V12.5.6

--- chopin-vendor.mk
+++ chopin-vendor.mk
@@ -26,6 +26,8 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/chopin/proprietary/lib/libvcodec_cap.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libvcodec_cap.so \
     vendor/xiaomi/chopin/proprietary/lib/libvcodec_capenc.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libvcodec_capenc.so \
     vendor/xiaomi/chopin/proprietary/lib/libvt_avsync.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libvt_avsync.so \
+    vendor/xiaomi/chopin/proprietary/lib64/android.hardware.boot@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/android.hardware.boot@1.0.so \
+    vendor/xiaomi/chopin/proprietary/lib64/android.hardware.boot@1.1.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/android.hardware.boot@1.1.so \
     vendor/xiaomi/chopin/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
     vendor/xiaomi/chopin/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
     vendor/xiaomi/chopin/proprietary/lib64/libmtk_vt_wrapper.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtk_vt_wrapper.so \
